d8ed87cf7567a2d470079add87635113a90a0074
Troop374 Raleigh <troop374raleigh@proton.me>
2022-11-20 10:55:14 -0500

    update eagles nest

---
+++ assets/js/troop374.js
@@ -0,0 +1,9 @@
+function toggleShow(elementId) {
+    let el = document.getElementById(elementId);
+    el.style.display = "block";
+  }
+  
+  function toggleHide(elementId) {
+    let el = document.getElementById(elementId);
+    el.style.display = "none";
+  }
