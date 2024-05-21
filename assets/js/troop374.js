1d2bab083190eac7b538895276620da27096c76b
Jaben <jaben@changemakerstudios.us>
2024-05-21 02:16:01 +0000

    Added Home images and a test link to see how they look.

--- assets/js/troop374.js
+++ assets/js/troop374.js
@@ -12,3 +12,12 @@ function showImageModal(imageSrc) {
   $('.imagepreview').attr('src', imageSrc);
   $('#imagemodal').modal('show');
 }
+
+$(document).ready(function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const testParam = urlParams.get('test');
+
+  if (testParam) {
+    $('#mast-head-bar').css('background-image', "url('assets/img/home/image" + testParam + ".jpg')")
+  }
+});
