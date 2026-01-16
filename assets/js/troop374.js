d3186f16fa3bb20501bab81d025a8bd5b4fb6967
Jaben <jaben@changemakerstudios.us>
2026-01-15 23:44:49 -0500

    Removed Eddie from the emails as per his request.

--- assets/js/troop374.js
+++ assets/js/troop374.js
@@ -13,8 +13,20 @@ function showImageModal(imageSrc) {
   $('#imagemodal').modal('show');
 }
 
+// Process email links at runtime - emails are hidden from HTML source until JS runs
+function initEmailLinks() {
+  document.querySelectorAll('.email-link').forEach(function(link) {
+    var emails = link.getAttribute('data-emails');
+    var subject = link.getAttribute('data-subject') || '';
+    if (emails) {
+      link.setAttribute('href', 'mailto:' + emails.replace(/,/g, '; ') + '?Subject=' + subject);
+    }
+  });
+}
+
 // for testing different mastheads
 $(document).ready(function () {
+  initEmailLinks();
   const urlParams = new URLSearchParams(window.location.search);
   const parallax = urlParams.get('parallax');
 
